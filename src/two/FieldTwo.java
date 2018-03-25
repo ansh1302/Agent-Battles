6742e0405bf209ad784c38baa1abac875d67fa5a
ansh1302 <ansh13027@gmail.com>
2018-03-25 19:09:10 -0400

    renamed fields appropriately

--- src/two/FieldTwo.java
+++ src/two/FieldTwo.java
@@ -7,7 +7,7 @@ public class FieldTwo {
     public static void main(String[] args) {
     	
     	//create frame and specify its characteristics
-        JFrame frame = new JFrame("Train Demo");
+        JFrame frame = new JFrame("Agent Battles");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         frame.getContentPane().setBackground(Color.WHITE);
